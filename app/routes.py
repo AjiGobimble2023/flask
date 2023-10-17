9d7e26917873702c2e87cff0efe823e4144a36b1
AjiGobimble2023 <ajimassaid.gobimbel@gmail.com>
2023-10-17 10:43:58 +0700

    Fix

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,10 @@
 from flask import Blueprint, jsonify, request
-from .user.controller import fetch_users, get_all_users, get_user, add_user, update_user, delete_user
+from .user.controller import  process_json
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    return 'Welcome to Akasia Test!'
+    return 'Welcome to IRT Test!'
 
-bp.route('/user/fetch', methods=['GET'])(fetch_users)
-bp.route('/users', methods=['GET'])(get_all_users)
-bp.route('/user/<int:user_id>', methods=['GET'])(get_user)
-bp.route('/user', methods=['POST'])(add_user)
-bp.route('/user/<int:user_id>', methods=['PUT'])(update_user)
-bp.route('/user/<int:user_id>', methods=['DELETE'])(delete_user)
+bp.route('/process_json', methods=['POST'])(process_json)
--- app/user/controller.py
+++ app/user/controller.py
@@ -1,156 +1,128 @@
 from datetime import datetime
+import random
 from flask import Blueprint, jsonify, request
 import requests
-from .models import User
-from app import db
+import pandas as pd
+import mpmath as mp
+import numpy as np
+import statistics
+from openpyxl import load_workbook
 
 
-def fetch_users():
-    page = request.args.get('page')
 
-    if not page:
-        return jsonify({'message': 'Missing query parameter: page'}), 400
+def process_json():
+    request_data = request.get_json()  # Mendapatkan data JSON dari permintaan POST
+    sample = []
+    noreg =[]
+    yakin =[]
+    nilaicareless = []
+    nilaibeda = []
+    success_data = []
 
-    try:
-        # Fetch data from the source URL
-        url = f'https://reqres.in/api/users?page={page}'
-        response = requests.get(url)
-        response_data = response.json()
+    for i in range(len(request_data)):
+        sample_isi = []
+        
+        noreg.append(request_data[i][f"c_Internal"])
+        for j in range(len(request_data[0].keys())-1):
+            key = f"c_N{str(j + 1)}"
+            if key in request_data[i]:
+                sampledata = request_data[i][key]
+            else:
+                sampledata = '0'
+            intdata = int(sampledata)
+            sample_isi.append(intdata)
+        sample.append(sample_isi)
+         
+        num_student = len(sample)
+        num_soal = len(sample[0])
+       
+        
+    for i in range(num_soal):
+            # Menghitung jumlah benar tiap nomor
+        success_count = sum(sample[i] == 1 for sample in sample)
+        success_data.append(success_count)
+            # Menghitung tingkat kesulitan tiap soal
+    successratio=[(success_data[j]/num_student) for j in range(num_soal)]
 
-        if 'data' not in response_data:
-            return jsonify({'message': 'Invalid response from the source URL'}), 500
+        # Menghitung total jawaban benar untuk setiap siswa
+    student_correct_counts = [sum(sample[i]) for i in range(num_student)]
+        # Mengurutkan siswa berdasarkan total jawaban benar
+    sorted_students = sorted(range(num_student), key=lambda i: student_correct_counts[i], reverse=True)
+        # Menghitung jumlah siswa terbaik dan terburuk
+    selected_students_count = int(0.30 * num_student)
+    top_students = sorted_students[:selected_students_count]
+    bottom_students = sorted_students[-selected_students_count:]
+        # Menghitung total jawaban benar per soal untuk siswa terbaik
+    correct_answers_top_students = [sum(sample[i][j] for i in top_students) for j in range(num_soal)]
+        # Menghitung total jawaban benar per soal untuk siswa terburuk
+    correct_answers_bottom_students = [sum(sample[i][j] for i in bottom_students) for j in range(num_soal)]
+        # Menghitung daya beda per soal
+        
 
-        fetched_users = response_data['data']
-
-        # Check if the fetched data already exists in the database
-        existing_user_ids = {user.id for user in User.query.all()}
-        new_users = [user for user in fetched_users if user['id'] not in existing_user_ids]
-
-        # Store the new users in the database
-        for user_data in new_users:
-            new_user = User(
-                id=user_data['id'],
-                email=user_data['email'],
-                first_name=user_data['first_name'],
-                last_name=user_data['last_name'],
-                avatar=user_data['avatar']
-            )
-            db.session.add(new_user)
-
-        db.session.commit()
-
-        return jsonify({'message': 'User data fetched and stored successfully', 'data': new_users}), 200
-
-    except requests.exceptions.RequestException as e:
-        return jsonify({'message': 'Error fetching data from the source URL'}), 500
-
-def get_all_users():
-    page = request.args.get('page', default=1, type=int)
-    items_per_page = request.args.get('per_page', default=6, type=int)
-
-    users = User.query.filter(User.deleted_at == None).paginate(page=page, per_page=items_per_page)
-
-    if not users.items:
-        return jsonify({'message': 'No users found'}), 404
-
-    result = []
-    for user in users.items:
-        result.append({
-            'id': user.id,
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'avatar': user.avatar
-        })
-
-    response = {
-        "page": page,
-        "per_page": items_per_page,
-        "total": users.total,
-        "total_pages": users.pages,
-        "data": result
-    }
-
-    return jsonify(response), 200
+    for i in range(num_soal):
+        hitung_pa = correct_answers_top_students[i] / selected_students_count
+        hitung_pb = correct_answers_bottom_students[i] / selected_students_count
+        hitung_beda = (hitung_pa - hitung_pb) if (hitung_pa - hitung_pb) != 0 else 0.01
+        nilaibeda.append(hitung_beda)
+            
+    nilaiguess = 0.2
+    awal = [sum(sample[i]) / num_soal for i in range(num_student)]
+    probawal=[]
+    for i in range(num_student):
+        prob_1 = []
+        for j in range(num_soal):
+            e = np.exp(-1*nilaibeda[j]*(awal[i]-successratio[j]))
+            hitung_prob = nilaiguess+((1-nilaiguess)/(1+e))
+            prob_1.append(hitung_prob)
+        probawal.append(prob_1)
+    iterasi = 10
+    for i in range(num_student) :
+     awalcalc = awal[i]
+    for _ in range (iterasi):
+        awalatas = []
+        awalbawah = []
+        for j in range(num_soal):
+         probcalc = probawal[i][j]
+        responsiswa = sample[i][j]
+        atas = nilaibeda[j]*(responsiswa-probcalc)
+        bawah = (nilaibeda[j]**2)*(probcalc-(1-probcalc))
+        if responsiswa == 0 :
+            atas = 0
+            bawah = 1
+        awalatas.append(atas)
+        awalbawah.append(bawah)
+        awalhitung = sum(awalatas)/sum(awalbawah)
+        jumlahawal = awalcalc+(awalhitung)
+        if abs(awalhitung) < 0.009:
+         break
+        awal[i]= jumlahawal
+        for j in range (num_soal):
+         e = np.exp((-1 * nilaibeda[j] * (awal[i] - successratio[j])))
+        probawal[i][j]= nilaiguess + ((1 - nilaiguess) * (1 / (1 + e)))
     
+    totalarray = []
+    for i in range(len(probawal)):
+        total_score = sum(probawal[i])/num_soal
+        totalarray.append(total_score)
+    nilairata = sum(totalarray)/num_student
 
-def get_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    elif user.deleted_at is not None:
-        return jsonify({'message': 'User has been deleted'}), 410  
-    else:
-        return jsonify({
-            'id': user.id,
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'avatar': user.avatar,
-            'created_at': user.created_at,
-            'updated_at': user.updated_at,
-            'deleted_at': user.deleted_at
-        }), 200
+    nilaiakhir = []
+    data = np.array(totalarray)
+    std =np.std(data)
+
+    standard_data = [float(i) for i in totalarray]
+    rerata = statistics.mean(standard_data)
+    list_varian = []
+    for bilangan in data:
+        list_varian.append(
+            (bilangan - rerata) ** 2
+        )
+
     
+    for i in range(num_student):
+        nilaiakhirhitung = 500+(100*(totalarray[i]-nilairata)/std)
+        nilaiakhir.append(nilaiakhirhitung)
 
-def add_user():
-    try:
-        data = request.get_json()
-        if not request.is_json:
-            return jsonify({'message': 'Invalid Content-Type. Please send JSON data.'}), 400
+    data = {'noreg': noreg, 'nilai_akhir': nilaiakhir}
 
-        email = data.get('email')
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        avatar = data.get('avatar')
-
-        if not email or not first_name or not last_name:
-            return jsonify({'message': 'Please provide email, first name, and last name'}), 400
-
-        new_user = User(email=email, first_name=first_name, last_name=last_name, avatar=avatar)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return jsonify({'message': 'User added successfully', 'created_at': new_user.created_at}), 201
-    except Exception as e:
-        print("Error:", str(e))
-        return jsonify({'message': 'An error occurred. Please check your request data.'}), 500
-
-def update_user(user_id):
-    data = request.get_json()
-
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
-
-    email = data.get('email')
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    avatar = data.get('avatar')
-
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    if avatar:
-        user.avatar = avatar
-
-    db.session.commit()
-
-    return jsonify({'message': 'User updated successfully'}), 200
-
-def delete_user(user_id):
-    authorization_header = request.headers.get('Authorization')
-    if authorization_header != 'Bearer 3cdcnTiBsl':
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
-    user.deleted_at = datetime.utcnow()
-    db.session.commit()
-    db.session.close() 
-
-    return jsonify({'message': 'User deleted successfully'}), 200
+    return jsonify({'message': data})
